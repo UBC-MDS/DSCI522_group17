793151aeb4b5c9bcd3fc670011ad32dd44b55679
Jason Chang <ja.chang0628@gmail.com>
2020-11-21 04:36:57 +0800

    data_download script completed

---
+++ src/data_download.py
@@ -0,0 +1,41 @@
+# author: Chun Chieh Chang, Pan Fan, Sakshi Jain
+# date: 2020-11-21
+
+"""This is a script that will take a url that contains the data and save it at a user defined path from the root of where the script is ran
+Usage: src/data_download.py --url=<url> --delim=<delim> --filepath=<filepath> --filename=<filename>
+
+Options:
+--url The url where the data is located
+--delim Delimiter that separates the data
+--filepath The user defined path to save the data
+--filenmae The user defined name to save the data
+"""
+import os
+import pandas as pd
+from docopt import docopt
+import requests
+
+opt = docopt(__doc__)
+
+
+def main(url, delim, filepath, filename):
+    # check if URL is valid
+    try:
+        req = requests.get(url)
+    except Exception as error:
+        print("Invalid URL, please enter a valid URL")
+        raise
+
+    data = pd.read_csv(url, delimiter=delim)
+    try:
+        pd.to_csv(filepath + filename, index=False)
+    except:
+        # create a new directory
+        print("path does not exist, creating path...")
+        os.makedirs(os.path.dirname(filepath))
+        data.to_csv(filepath + filename, index=False)
+
+
+if __name__ == "__main__":
+    main(opt["--url"], opt["--delim"], opt["--filepath"], opt["--filename"])
+
